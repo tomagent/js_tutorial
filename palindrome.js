9ba147afa086cffb3d900a39b36492ac455fcd96
tomagent <tommymora2003@gmail.com>
2021-04-26 23:24:55 -0500

    prototypes

--- palindrome.js
+++ palindrome.js
@@ -3,9 +3,6 @@ function reverse(str) {
   return Array.from(str).reverse().join("");
 }
 
-
-
-
 // Return an array of the username and domain for a standard email address
 function emailParts(email) {
   let lowerCaseEmail = email.toLowerCase();
@@ -16,14 +13,30 @@ function emailParts(email) {
 function Phrase(content) {
   this.content = content;
 
-  // Makes the phrase LOUDER.
-  this.louder = function() {
-    return this.content.toUpperCase();
-  }
+  this.processor = function(string) {
+  return this.string.toLowerCase();
+​  }
+
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+   return this.processor(this.content);
+ }
 
   // Returns true for a palindrome, false otherwise
   this.palindrome = function palindrome() {
-    let processedContent = this.content.toLowerCase();
-    return processedContent === reverse(processedContent);
+   return this.processedContent() === reverse(this.processedContent());
   }
 }
+
+// Defines a TranslatedPhrase object.
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.processor(this.translation);
+  }
+}
+
+TranslatedPhrase.prototype = new Phrase();
